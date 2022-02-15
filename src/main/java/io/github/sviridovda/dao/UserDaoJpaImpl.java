37d8dca67a4e3721430a765a5640653b42ad415f
Dmitrii Sviridov <dm.sviridov99@yandex.ru>
2022-02-15 14:33:59 +0300

    fix JPAdao

--- src/main/java/io/github/sviridovda/dao/UserDaoJpaImpl.java
+++ src/main/java/io/github/sviridovda/dao/UserDaoJpaImpl.java
@@ -11,42 +11,47 @@ import java.util.List;
 @Component
 public class UserDaoJpaImpl implements UserDao{
     private final EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
 
     public UserDaoJpaImpl(EntityManagerFactory entityManagerFactory) {
         this.entityManagerFactory = entityManagerFactory;
-        entityManager = entityManagerFactory.createEntityManager();
     }
 
     @Override
     public List<User> getAllUsers() {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         Query query = entityManager.createQuery("SELECT u FROM User u");
         List<User> res = query.getResultList();
         transaction.commit();
+        entityManager.close();
         return res;
     }
 
     @Override
     public User getUserById(long id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         User res = entityManager.find(User.class, id);
         transaction.commit();
+        entityManager.close();
         return res;
     }
 
     @Override
     public void addUser(User user) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         entityManager.persist(user);
         transaction.commit();
+        entityManager.close();
     }
 
     @Override
     public void update(long id, User user) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         User userToUpdate = entityManager.find(User.class, id);
@@ -55,13 +60,16 @@ public class UserDaoJpaImpl implements UserDao{
         userToUpdate.setName(user.getName());
         userToUpdate.setAge(user.getAge());
         transaction.commit();
+        entityManager.close();
     }
 
     @Override
     public void deleteUser(long id) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
         entityManager.remove(entityManager.find(User.class, id));
         transaction.commit();
+        entityManager.close();
     }
 }
